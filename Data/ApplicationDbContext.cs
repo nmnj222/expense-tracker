450f2053077dffd4b01bb95b007be4a9a10cc127
Nemanja Vlaovic <nemanja.vlaovic@vega.local>
2026-08-19 11:08:35 +0200

    Made DbContext, User model entity.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expense_tracker.Models
+{
+    [Table("User")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set;  }
+        public string username {  get; set; }
+        public string password { get; set; }    
+        public string email { get; set;  }
+        public bool isPremium { get; set; }
+
+        public DateTime CreatedAt { get; set;  }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
+
+    }
+}
