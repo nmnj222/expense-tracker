239774982b5e08e16f268adc331f74edbf6c82ab
Nemanja Vlaovic <nemanja.vlaovic@vega.local>
2026-08-26 11:31:01 +0200

    Added custom attribute validator for checking if int gt 0

--- Dtos/CreateTransactionGroupDto.cs
+++ Dtos/CreateTransactionGroupDto.cs
@@ -1,4 +1,5 @@
 ﻿using ExpenseTracker.Enums;
+using ExpenseTracker.Validators;
 using System.ComponentModel.DataAnnotations;
 
 namespace ExpenseTracker.Dtos
@@ -8,7 +9,7 @@ namespace ExpenseTracker.Dtos
         [Required(AllowEmptyStrings = false, ErrorMessage = "Group name is required")]
         public required string Name { get; set; }
 
-        [Range(0, int.MaxValue)]
+        [IsGreaterThanZero]
         public int MonthlyCap { get; set; }
 
         [Required]
---
+++ Validators/IsGreaterThanZeroAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Validators;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class IsGreaterThanZeroAttribute : ValidationAttribute
+{
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (IsValid(value))
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult($"Value {value} is not greater than 0", [validationContext.MemberName!]);
+    }
+
+    public override bool IsValid(object? value)
+    {
+        if (value is not int)
+        {
+            return false;
+        }
+
+        if (!IsGreaterThanZero((int)value))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private static bool IsGreaterThanZero(int number)
+    {
+        return number > 0;
+    }
+}
