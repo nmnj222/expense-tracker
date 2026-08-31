7f8e9868ed1f1fc626ea671448c2b1cc594eaeb4
Nemanja Vlaovic <nemanja.vlaovic@vega.local>
2026-08-31 14:08:52 +0200

    Background job to process scheduled transactions

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using ExpenseTracker.Interfaces;
+﻿using Coravel;
+using ExpenseTracker.Interfaces;
+using ExpenseTracker.Jobs;
 using ExpenseTracker.Services;
 
 namespace ExpenseTracker.Extensions;
@@ -9,12 +11,14 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAuthorization();
+        services.AddScheduler();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<ITransactionGroupService, TransactionGroupService>();
         services.AddScoped<ITransactionService, TransactionService>();
         services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddTransient<ProcessScheduledTransactions>();
 
         return services;
     }
---
+++ Jobs/ProcessScheduledTransactions.cs
@@ -0,0 +1,56 @@
+﻿using Coravel.Invocable;
+using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Jobs;
+
+public class ProcessScheduledTransactions(ApplicationDbContext _context, ILogger<ProcessScheduledTransactions> _logger) : IInvocable
+{
+    public async Task Invoke()
+    {
+        _logger.LogInformation("Logger running.");
+        DateTime now = DateTime.UtcNow;
+
+        var scheduledTransactions = await _context.ScheduledTransactions
+            .Where(st => st.ScheduledAt <= now  && st.Status == Enums.ScheduledTransactionStatus.Scheduled)
+            .ToListAsync();
+
+        if (scheduledTransactions.Count == 0) return;
+
+        foreach (var scheduled in scheduledTransactions)
+        {
+            scheduled.Status = Enums.ScheduledTransactionStatus.InProgress;
+            await _context.SaveChangesAsync();
+
+            using var dbTransaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+
+                Transaction transaction = new Transaction
+                {
+                    Amount = scheduled.Amount,
+                    TransactionGroupId = scheduled.TransactionGroupId,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                _context.Add(transaction);
+
+                scheduled.Status = Enums.ScheduledTransactionStatus.Executed;
+
+                await _context.SaveChangesAsync();
+                await dbTransaction.CommitAsync();
+
+            }
+            catch (Exception e)
+            {
+                await dbTransaction.RollbackAsync();
+                _logger.LogError(e, "Failed to execute scheduled transaction {Id}", scheduled.Id);
+
+                scheduled.Status = Enums.ScheduledTransactionStatus.Failed;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
