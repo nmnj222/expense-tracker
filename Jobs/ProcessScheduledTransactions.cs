23cd8da40fe3904965487ad5b82a78eaac37bc69
Nemanja Vlaovic <nemanja.vlaovic@vega.local>
2026-09-03 12:19:41 +0200

    fix: Modified scheduled transaction processor to drop unnecessary transaction interface; fixed generic logging; Removed unnecessary status updates

--- Jobs/ProcessScheduledTransactions.cs
+++ Jobs/ProcessScheduledTransactions.cs
@@ -9,48 +9,35 @@ public class ProcessScheduledTransactions(ApplicationDbContext _context, ILogger
 {
     public async Task Invoke()
     {
-        _logger.LogInformation("Logger running.");
+        _logger.LogInformation("Initiated processing scheduled transactions");
         DateTime now = DateTime.UtcNow;
 
         var scheduledTransactions = await _context.ScheduledTransactions
-            .Where(st => st.ScheduledAt <= now  && st.Status == Enums.ScheduledTransactionStatus.Scheduled)
+            .Where(st => st.ScheduledAt <= now && st.Status == Enums.ScheduledTransactionStatus.Scheduled)
             .ToListAsync();
 
-        if (scheduledTransactions.Count == 0) return;
+        if (scheduledTransactions.Count == 0)
+        {
+            return;
+        }
 
         foreach (var scheduled in scheduledTransactions)
         {
-            scheduled.Status = Enums.ScheduledTransactionStatus.InProgress;
+
+
+            Transaction transaction = new Transaction
+            {
+                Amount = scheduled.Amount,
+                TransactionGroupId = scheduled.TransactionGroupId,
+                CreatedAt = now
+            };
+
+            _context.Add(transaction);
+
+            scheduled.Status = Enums.ScheduledTransactionStatus.Executed;
+
             await _context.SaveChangesAsync();
-
-            using var dbTransaction = await _context.Database.BeginTransactionAsync();
-
-            try
-            {
-
-                Transaction transaction = new Transaction
-                {
-                    Amount = scheduled.Amount,
-                    TransactionGroupId = scheduled.TransactionGroupId,
-                    CreatedAt = DateTime.UtcNow
-                };
-
-                _context.Add(transaction);
-
-                scheduled.Status = Enums.ScheduledTransactionStatus.Executed;
-
-                await _context.SaveChangesAsync();
-                await dbTransaction.CommitAsync();
-
-            }
-            catch (Exception e)
-            {
-                await dbTransaction.RollbackAsync();
-                _logger.LogError(e, "Failed to execute scheduled transaction {Id}", scheduled.Id);
-
-                scheduled.Status = Enums.ScheduledTransactionStatus.Failed;
-                await _context.SaveChangesAsync();
-            }
+            _logger.LogInformation($"Succesfully processed scheduled transaction with id: {scheduled.Id}");
         }
     }
 }
